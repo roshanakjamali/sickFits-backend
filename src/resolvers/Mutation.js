3e221cb75dbf39387628a2f9565e7443b27bff53
Roshanak <roshanakjamali@gmail.com>
2019-12-23 22:22:46 +0330

    items, pagination

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -8,10 +8,31 @@ const Mutations = {
       },
       info
     );
-
-    console.log(item);
-
     return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    //first copy of the updates
+    const updates = { ...args };
+    // remove the ID
+    delete updates.id;
+    //run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1.find the item
+    const item = await ctx.db.query.item({ where }, `{ id title}`);
+    // 2.check if the own that item, or have the permission
+    // 3.delete it
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
